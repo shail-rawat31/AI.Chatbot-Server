74c90c1d958b09772e249c983846b73e89031731
Shail Rawat <22BCA10668@cuchd.in>
2026-07-11 02:00:41 +0530

    Delete src directory

--- src/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { handleChat } from "../controllers/chat.controller.js";
-
-const router = Router();
-
-router.post("/chat", handleChat);
-
-export default router;
